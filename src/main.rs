bb6236e4b23b5920fd5fa58fffeadbcd80be9c1a
Sam Szuflita <sam.szuflita@gmail.com>
2020-11-08 19:15:31 -0500

    Add jump support

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,20 @@ struct Velocity(Vec2);
 
 struct Platform;
 
+struct Jumps(usize);
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
         .add_system(gravity_system.system())
+        .add_system(jump_system.system())
         .add_system(position_system.system())
         .add_system(bevy::input::system::exit_on_esc_system.system())
         .run();
 }
+
+const JUMP_COUNT: usize = 3;
 
 fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands
@@ -32,6 +37,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
             ..Default::default()
         })
         .with(Velocity(Vec2::default()))
+        .with(Jumps(JUMP_COUNT))
         .with(Player);
 
     commands
@@ -47,10 +53,10 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
 const GRAVITY_ACCELERATION: f32 = -1.0;
 
 fn gravity_system(
-    mut query: Query<(&Player, &Sprite, &Transform, &mut Velocity)>,
+    mut query: Query<(&Player, &Sprite, &Transform, &mut Velocity, &mut Jumps)>,
     collider_query: Query<(&Transform, &Sprite)>,
 ) {
-    for (_player, player_sprite, player_transform, mut velocity) in query.iter_mut() {
+    for (_player, player_sprite, player_transform, mut velocity, mut jumps) in query.iter_mut() {
         let mut falling = true;
 
         for (transform, sprite) in collider_query.iter() {
@@ -65,6 +71,7 @@ fn gravity_system(
                 println!("Player: {:?}", player_transform);
                 *velocity.0.y_mut() = 0.0;
                 falling = false;
+                jumps.0 = JUMP_COUNT;
             }
         }
 
@@ -72,6 +79,21 @@ fn gravity_system(
             *velocity.0.y_mut() += GRAVITY_ACCELERATION;
         }
     }
+}
+
+fn jump_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Jumps, &mut Velocity)>) {
+        if !keyboard_input.just_pressed(KeyCode::Space) {
+            return;
+        }
+
+        for (mut jump, mut velocity) in query.iter_mut() {
+            if jump.0 > 0 {
+                jump.0 -= 1;
+                *velocity.0.y_mut() += 100.0;
+            }
+        }
 }
 
 fn position_system(time: Res<Time>, mut query: Query<(&Velocity, &mut Transform)>) {
